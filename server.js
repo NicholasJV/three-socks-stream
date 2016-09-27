2737207e9c4c2558fb5e63da6d30507fa0ba3ddf
Nicholas VanVoorthuysen <nicholas.vanvoorthuysen@gmail.com>
2016-09-27 19:45:46 -0400

    improved server file org, added chalk module for better console logging

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-
 var path = require('path');
 var http = require('http');
 var server = http.createServer();
+var chalk = require('chalk');
+var socketIO = require('socket.io');
+
 var express = require('express');
 var app = express();
-var socketio = require('socket.io');
 
-// This will send typical CRUD requests down to
-// our express server
 server.on('request', app);
 
-var io = socketio(server);
+// =-=-=-=-=-=-= Socket connect init =-=-=-=-=-=-=-=
 
-// will assign device orientation data to oData:
+// device orientation data:
+var io = socketIO(server);
 var oData;
-
 io.on('connection', function(socket){
-	console.log('a new client has connected to the socket with id:', socket.id);
-	// trying to accept the phone data?
+	console.log(chalk.yellow('a new client has connected'), '(ID: '+socket.id+')');
 	socket.on('orientationData', function(data){
 		oData = data;
 		socket.broadcast.emit('broadcastData', data)
@@ -26,20 +24,21 @@ io.on('connection', function(socket){
 
 if (oData){
 	setInterval(function(){console.log("from server: ", oData)}, 1000);
-} else { console.log('no data has made it to the server yet') }
+} else { console.log('...waiting for data from the server...') }
+
+// =-=-=-=-=-=-= Simple server routes =-=-=-=-=-=-=-=
 
 app.use(express.static(path.join(__dirname, 'scripts')));
 app.use(express.static(path.join(__dirname, 'browser')));
-app.use(express.static(path.join(__dirname, 'fourLibrary')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/detect', function (req, res){
+app.get('/control', function (req, res){
 	res.sendFile(path.join(__dirname, 'browser/detect.html'))
 })
 
 server.listen(8080, function () {
-  console.log('The server is listening on port 8080!');
+  console.log(chalk.green('The server is listening on port 8080!'));
 });
